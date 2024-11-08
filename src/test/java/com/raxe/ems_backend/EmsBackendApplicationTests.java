909c3d776abb59c7eb55cf6921c27f8380e5e787
Harifidy Desk <harifidyilay@gmail.com>
2024-11-08 17:19:50 -0500

    Init Configuration

---
+++ src/test/java/com/raxe/ems_backend/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxe.ems_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
