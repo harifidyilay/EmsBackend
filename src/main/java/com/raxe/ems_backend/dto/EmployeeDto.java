e3f5b4f5dcfbf792f8e35bcbc6ce6f17f9294d85
Harifidy Desk <harifidyilay@gmail.com>
2024-11-08 17:59:13 -0500

    Create EmployeeDto -  to handle transfert data

---
+++ src/main/java/com/raxe/ems_backend/dto/EmployeeDto.java
@@ -0,0 +1,29 @@
+package com.raxe.ems_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// DTO (Data Transfer Object) is a simple Java class used to transfer data
+// between different layers of an application | Specificaly Between Client - Server | in Controller
+// - Simple Structure - Immutable - Serialization
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmployeeDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
+
+//    Benefits of Using DTOs:
+//        Decoupling Layers: DTOs help decouple the presentation layer from the business logic layer, making the application more modular and maintainable.
+//        Performance Improvement: By transferring only the required data, DTOs can reduce the overhead of network calls and improve performance.
+//        Data Security: DTOs can be used to expose only specific fields to the client, ensuring sensitive data is not leaked.
+//
+//    In Spring applications, DTOs are often used in conjunction with controllers and services to handle data transfer between the client and the server. If you need more details or specific examples, feel free to ask
+//
+
