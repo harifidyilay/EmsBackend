09bdf1e3fd66af2b99def892ed8020f98b92740d
Harifidy Desk <harifidyilay@gmail.com>
2024-11-21 22:30:28 -0500

    Spring boot slf4j rolling logging

--- src/main/java/com/raxe/ems_backend/aspect/EmployeeServiceImplAspect.java
+++ src/main/java/com/raxe/ems_backend/aspect/EmployeeServiceImplAspect.java
@@ -3,6 +3,8 @@ package com.raxe.ems_backend.aspect;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -10,14 +12,17 @@ import java.time.LocalDateTime;
 @Aspect
 @Component
 public class EmployeeServiceImplAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImplAspect.class);
+
     @Before(value="execution(* com.raxe.ems_backend.service.impl.EmployeeServiceImpl.getAllEmployees(..))")
     public void beforeGetAllEmployees(JoinPoint joinPoint) {
-        System.out.println(LocalDateTime.now().toString() + ": Before getAllEmployees() :"+ joinPoint.getSignature());
+        logger.info("Before getAllEmployees() :"+ joinPoint.getSignature());
     }
 
     @After(value="execution(* com.raxe.ems_backend.service.impl.EmployeeServiceImpl.getAllEmployees(..))")
     public void afterGetAllEmployees(JoinPoint joinPoint) {
-        System.out.println(LocalDateTime.now().toString() +": After getAllEmployees() :"+ joinPoint.getSignature());
+        logger.info("After getAllEmployees() :"+ joinPoint.getSignature());
     }
 
     @Pointcut(value= "execution(* com.raxe.ems_backend.service.impl.EmployeeServiceImpl.getAllEmployees(..))")
@@ -29,7 +34,7 @@ public class EmployeeServiceImplAspect {
     @Around(value= "logDisplayingAllEmployees()")
     public void aroundAdvice(ProceedingJoinPoint jp) throws Throwable
     {
-        System.out.println(LocalDateTime.now().toString()+" The method aroundAdvice() before invokation of the method " + jp.getSignature().getName() + " method");
+        logger.info("The method aroundAdvice() before invokation of the method " + jp.getSignature().getName() + " method");
         try
         {
             jp.proceed();
@@ -38,7 +43,8 @@ public class EmployeeServiceImplAspect {
         {
 
         }
-        System.out.println(LocalDateTime.now().toString()+" The method aroundAdvice() after invokation of the method " + jp.getSignature().getName() + " method");
+        logger.info("The method aroundAdvice() after invokation of the method " + jp.getSignature().getName() + " method");
+
     }
 
 }
